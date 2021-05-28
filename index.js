542173450cfb53d7494ea0ca1ad5806af2dd4926
Rohan Gupta <guptarohan369@gmail.com>
2021-05-28 23:55:18 +0530

    Add mongoose and uuid

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { v4 as uuidv4, validate as uuidValidate, version as uuidVersion } from 'uuid';
 
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const port = process.env.PORT || 5050;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/swoosh';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on('error', err => console.error(err));
+db.once('open', () => console.log('> Database connection established'));
+
+const uuidValidateV4 = (uuid) => uuidValidate(uuid) && uuidVersion(uuid) === 4;
+
+const MessageSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    _id: String,
+});
+
+const Message = mongoose.model('message', MessageSchema);
 
 app.listen(port, () => `> Serving on PORT: ${port}`);
 
-app.get('/', (req, res) => {
-    res.send('Greetings from Swoosh!');
+app.get('/', async (req, res) => {
+    const content = `Lorem ipsum amet minim adipisicing excepteur amet sit incididunt do laborum. Et excepteur et ipsum mollit tempor do reprehenderit ullamco minim.`;
+    const _id = uuidv4();
+    const message = new Message({ _id, content });
+    await message.save();
+    res.send(message);
+});
+
+app.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    if (id && uuidValidateV4(id)) {
+        const message = await Message.findById(id);
+        res.send(message);
+    } else {
+        res.send('Invalid ID');
+    }
 });

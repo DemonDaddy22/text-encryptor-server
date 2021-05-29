1a4e735b63e40db0173fbabf881cfeec7698d1bc
Rohan Gupta <guptarohan369@gmail.com>
2021-05-29 15:50:25 +0530

    Add mongoose-encryption plugin on mongoose schema

--- models/Message.js
+++ models/Message.js
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import mongoose from 'mongoose';
+import encrypt from 'mongoose-encryption';
 
 const Schema = mongoose.Schema;
 
@@ -20,6 +24,13 @@ const MessageSchema = new Schema({
         type: Number,
         required: true,
     }
+});
+
+MessageSchema.plugin(encrypt, {
+    encryptionKey: process.env.ENCRYPTION_KEY,
+    signingKey: process.env.SIGN_KEY,
+    encryptedFields: ['content'],
+    decryptPostSave: false,
 });
 
 const Message = mongoose.model('message', MessageSchema);
